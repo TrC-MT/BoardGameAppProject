76f242fadb6d1414cb36217cbf88fb27afc9314f
Travis Cook <travis.cook5583@stu.mtec.edu>
2023-05-17 08:39:00 -0600

    Finished walkthrough 9.

--- src/app/modules/app-routing.module.ts
+++ src/app/modules/app-routing.module.ts
@@ -2,10 +2,12 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { GameSearchComponent } from '../components/game-search/game-search.component';
 import { UserSelectionsComponent } from '../components/user-selections/user-selections.component';
+import { GameDetailsComponent } from '../components/game-details/game-details.component';
 
 const routes: Routes = [
   { path: '', redirectTo: 'search', pathMatch: 'full' }, // "When the application first loads to localhost:4200 that it automatically redirects to localhost:4200/search. And this redirect only happens if the exact url is localhost:4200 because of the pathMatch: 'full' property".
   { path: 'search', component: GameSearchComponent },
+  { path: 'game-details', component: GameDetailsComponent },
   { path: 'owned', component: UserSelectionsComponent },
   { path: 'wishlist', component: UserSelectionsComponent },
 ];
